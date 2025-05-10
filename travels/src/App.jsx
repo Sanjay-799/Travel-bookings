bf27d04758e5fd653798f9d5d2cb2fbef9541614
Sanjay <sanjaydasari799@gmail.com>
2025-05-10 19:11:32 +0530

    Initial commit - Travel Booking Web App

---
+++ travels/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegisterForm from './components/RegisterForm'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Welcome</h1>
+      <RegisterForm/>
+    </div>
+  )
+}
+
+export default App
